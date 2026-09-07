22acdafb53fcad95c5e81a0f7ee44396e22e35e8
anasoohero-cmyk <anasoohero@gmail.com>
2026-09-07 14:21:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='veronza-shell-v34';
+const CACHE_NAME='veronza-shell-v36';
 const SHELL=['./','./index.html','./app.js','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
