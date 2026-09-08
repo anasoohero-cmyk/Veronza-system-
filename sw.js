b4e920be103a8d4604ec296d69710f664845de11
anasoohero-cmyk <anasoohero@gmail.com>
2026-09-08 16:04:20 +0200

    Restore service worker after audit

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME='V88';
 // V77 real update progress
-// import repair release trigger
 const SHELL=['./','./index.html','./app.js','./sw.js','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
